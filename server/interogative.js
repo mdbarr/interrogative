dfc556740fdc13bdc139553dc34a1bc414fd9f31
Mark Barr <mark@hyperingenuity.com>
2019-08-10 17:11:44 -0400

    interogative: server cors and environment improvements

--- server/interogative.js
+++ server/interogative.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 'use strict';
 
-const restify = require('restify');
 const pty = require('node-pty');
+const restify = require('restify');
 const Watershed = require('watershed').Watershed;
+const corsMiddleware = require('restify-cors-middleware');
 
 const api = restify.createServer({
   name: 'Interogative',
@@ -14,6 +15,19 @@ const api = restify.createServer({
 
 api.ws = new Watershed();
 
+/// ///////
+
+const cors = corsMiddleware({
+  origins: [ '*' ],
+  allowHeaders: [ 'Authorization' ],
+  exposeHeaders: [ 'Authorization' ]
+});
+
+api.pre(cors.preflight);
+api.use(cors.actual);
+
+/// ///////
+
 api.use(restify.pre.sanitizePath());
 api.pre(restify.plugins.pre.dedupeSlashes());
 api.use(restify.plugins.dateParser());
@@ -21,16 +35,13 @@ api.use(restify.plugins.queryParser());
 api.use(restify.plugins.bodyParser());
 api.use(restify.plugins.authorizationParser());
 
+/// ///////
+
 api.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
-  res.header('Access-Control-Allow-Headers', [
-    'Access-Control-Allow-Headers', 'Origin', 'Accept', 'X-Requested-With',
-    'Content-Type', 'Access-Control-Request-Method', 'Access-Control-Request-Headers',
-    'Authorization' ].join(', '));
   next();
 });
+
+/// ///////
 
 api.get('/attach/shell', (req, res, next) => {
   if (!res.claimUpgrade) {
@@ -38,15 +49,20 @@ api.get('/attach/shell', (req, res, next) => {
     return;
   }
 
-  console.log(req.query);
-
   const upgrade = res.claimUpgrade();
   const shed = api.ws.accept(req, upgrade.socket, upgrade.head);
 
-  const shell = pty.spawn('/bin/bash', [], {
+  const shell = pty.spawn('/bin/bash', [ ], {
     name: 'xterm-256color',
-    cwd: process.env.PWD,
-    env: process.env,
+    cwd: process.env.HOME,
+    env: {
+      HOME: process.env.HOME,
+      USER: process.env.USER,
+      LANG: process.env.LANG,
+      PATH: process.env.PATH,
+
+      INTEROGATIVE: '1'
+    },
     cols: Number(req.query.cols) || 100,
     rows: Number(req.query.rows) || 24
   });
@@ -71,6 +87,8 @@ api.get('/attach/shell', (req, res, next) => {
     shell.kill();
   });
 });
+
+/// //////
 
 api.listen(3169, () => {
   console.log('Interogative listening on http://0.0.0.0:3169');
