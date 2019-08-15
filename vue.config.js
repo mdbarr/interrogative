682c7503fdc0702e5542065de5f0c713678ce65a
Mark Barr <mark@hyperingenuity.com>
2019-08-15 10:03:13 -0400

    interrogative: webpack dev server config fix for hmr over the network

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 const config = require('./defaults');
 
-module.exports = { devServer: { proxy: {
+module.exports = { devServer: {
+  host: '0.0.0.0',
+  public: '0.0.0.0:8080',
+  disableHostCheck: true,
+  proxy: {
   '^/api': { target: `http://localhost:${ config.port }` },
   '^/ws': {
     target: `ws://localhost:${ config.container.port }`,
