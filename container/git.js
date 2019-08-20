6bcc05febfc3ff9dc8034f616af040915d98d5e2
Mark Barr <mark@hyperingenuity.com>
2019-08-20 13:51:46 -0400

    git: repository changes debounced

--- container/git.js
+++ container/git.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const chokidar = require('chokidar');
 const Griff = require('@mdbarr/griff');
+const { debounce } = require('barrkeep/utils');
 
 function Git (container, options) {
   const griff = new Griff({
@@ -14,19 +15,6 @@ function Git (container, options) {
 
   const directory = path.join(options.repository, '/.git');
   let cached = '';
-
-  this.watch = () => {
-    console.log('*generating git watcher:', directory);
-
-    this.watcher = chokidar.watch(directory, {
-      persistent: true,
-      ignoreInitial: true
-    });
-
-    this.watcher.on('all', (type, filename) => {
-      this.render(false);
-    });
-  };
 
   this.render = (useCache = true) => {
     if (useCache && cached) {
@@ -43,6 +31,21 @@ function Git (container, options) {
         });
       });
     }
+  };
+
+  const onAll = debounce(() => {
+    this.render(false);
+  }, 500);
+
+  this.watch = () => {
+    console.log('*generating git watcher:', directory);
+
+    this.watcher = chokidar.watch(directory, {
+      persistent: true,
+      ignoreInitial: true
+    });
+
+    this.watcher.on('all', onAll);
   };
 
   this.start = () => {
