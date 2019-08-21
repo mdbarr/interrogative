af67b79ed28c497ff8b59a72ab3bd4a79332b00d
Mark Barr <mark@hyperingenuity.com>
2019-08-21 11:52:57 -0400

    interrogative: ui tweaks

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,6 +1,11 @@
 <template>
 <div>
   Settings<br><br>
+
+  <div>
+    <span class="font-weight-bold">Editor</span><v-icon class="float-right">mdi-circle-edit-outline</v-icon>
+  </div>
+  <br>
   <v-select
     v-model="state.theme"
     :items="state.settings.themes"
@@ -18,6 +23,12 @@
     label="Key Map"
     class="caption">
   </v-select>
+  <br><br>
+  <div>
+    <span class="font-weight-bold">Terminal</span><v-icon class="float-right">mdi-console</v-icon>
+  </div>
+  <br>
+
 </div>
 </template>
 
@@ -33,5 +44,7 @@ export default {
 </script>
 
 <style>
-
+.float-right {
+    float: right;
+}
 </style>
