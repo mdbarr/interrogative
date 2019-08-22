e5e188cd9f79596ead0aac1191eefba154651629
Mark Barr <mark@hyperingenuity.com>
2019-08-22 10:12:36 -0400

    manager: refactor for better layout, metadata split into own service

--- defaults.js
+++ defaults.js
@@ -1,12 +1,16 @@
 module.exports = {
   name: 'Interrogative',
   manager: {
-    port: 1312,
+    port: 1311,
     host: '0.0.0.0',
     interviews: './interviews'
   },
+  metadata: {
+    port: 1312,
+    host: '0.0.0.0'
+  },
   container: {
-    port: 1311,
+    port: 1313,
     host: '0.0.0.0',
     home: process.env.INTERROGATIVE_HOME || process.env.HOME,
     open: [
