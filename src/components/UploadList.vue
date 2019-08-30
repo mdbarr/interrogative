3257adc48b7682fef27ff9928f29afbd6698d3f2
Mark Barr <mark@hyperingenuity.com>
2019-08-30 14:12:34 -0400

    interrogative: upload list prototyping

--- src/components/UploadList.vue
+++ src/components/UploadList.vue
@@ -1,12 +1,27 @@
 <template>
 <div class="pt-1">
   <div v-for="upload of state.uploads" :key="upload.name">
-    <v-icon small>mdi-cloud-upload</v-icon>
-    <span class="uploading pl-2">{{ upload.name }}</span>
-    <div class="progress-holder">
-      <v-progress-linear v-model="upload.progress" v-if="upload.progress < 100" height="2">
-      </v-progress-linear>
-    </div>
+    <v-tooltip bottom nudge-top="18">
+      <template v-slot:activator="{ on }">
+        <div v-on="on">
+          <v-icon small>mdi-cloud-upload</v-icon>
+          <span class="uploading pl-2">{{ upload.name }}</span>
+          <div class="progress-holder">
+            <v-progress-linear
+              v-if="upload.progress < 100"
+              v-model="upload.progress"
+              height="2"
+              buffer-value="0"
+              stream
+              >
+            </v-progress-linear>
+          </div>
+        </div>
+      </template>
+      {{ upload.name }}<br>
+      {{ upload.size }}<br>
+      {{ upload.type }}
+    </v-tooltip>
   </div>
 </div>
 </template>
