c1fa59fa05737f84e7d13dd8c245093cc99c0332
Mark Barr <mark@hyperingenuity.com>
2019-09-23 17:40:05 -0400

    interrogative: remove unnecessary async queue, cleanup process start error handling

--- bin/cli.js
+++ bin/cli.js
@@ -5,18 +5,25 @@
 const fs = require('fs');
 process.env.TZ = fs.readFileSync('/etc/timezone').toString().trim();
 
+const handler = (error) => {
+  if (error) {
+    console.log('Encountered error', error);
+    process.exit(1);
+  }
+};
+
 // Handle command line options
 const argv = require('yargs').argv;
 if (argv.manager) {
   const Manager = require('../lib/manager/manager');
   const manager = new Manager({ options: argv });
-  manager.start();
+  manager.start(handler);
 } else if (argv.emailer) {
   const Emailer = require('../lib/emailer/emailer');
   const emailer = new Emailer({ options: argv });
-  emailer.start();
+  emailer.start(handler);
 } else if (argv.container) {
   const Container = require('../lib/container/container');
   const container = new Container({ options: argv });
-  container.start();
+  container.start(handler);
 }
