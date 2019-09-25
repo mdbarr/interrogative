589817de344fe613d9178f6b5e0029f97a567346
Mark Barr <mark@hyperingenuity.com>
2019-09-25 13:21:25 -0400

    mongo: refactored oplog for simplicty and better control

--- defaults.js
+++ defaults.js
@@ -17,7 +17,10 @@ module.exports = {
     url: 'mongodb://mongo:27017/interrogative'
   },
   sessions: { cookie: 'dapper-session' },
-  oplog: { url: 'mongodb://mongo:27017/local' },
+  oplog: {
+    retry: 5000,
+    url: 'mongodb://mongo:27017/local'
+  },
   metadata: {
     port: 1312,
     host: '0.0.0.0'
