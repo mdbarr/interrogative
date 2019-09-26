dfcb1a17dbb67c26e50f86331a4ca0ae965f2b8c
Mark Barr <mark@hyperingenuity.com>
2019-09-26 10:14:19 -0400

    messenger: more spelling fixes

--- bin/cli.js
+++ bin/cli.js
@@ -19,8 +19,8 @@ if (argv.manager) {
   const manager = new Manager({ options: argv });
   manager.start(handler);
 } else if (argv.messenger) {
-  const Messanger = require('../lib/messenger/messenger');
-  const messenger = new Messanger({ options: argv });
+  const Messenger = require('../lib/messenger/messenger');
+  const messenger = new Messenger({ options: argv });
   messenger.start(handler);
 } else if (argv.container) {
   const Container = require('../lib/container/container');
