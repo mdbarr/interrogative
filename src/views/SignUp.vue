b8c91efa28bbc9c43602d03d213f3e787627ebea
Mark Barr <mark@hyperingenuity.com>
2019-09-30 21:09:08 -0400

    sign-up: basic input validation

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -9,7 +9,7 @@
             INTERROGATIVE.IO - SIGN UP
           </div>
           <v-card-text>
-            <v-form>
+            <v-form ref="form">
               <v-text-field
                 autofocus
                 label="Full Name *"
@@ -18,7 +18,7 @@
                 v-model="name"
                 ref="name"
                 @keyup.enter="pressEnter"
-                width="100"
+                :rules="[ validateName ]"
                 ></v-text-field>
 
               <v-text-field
@@ -28,6 +28,7 @@
                 v-model="email"
                 ref="email"
                 @keyup.enter="pressEnter"
+                :rules="[ validateEmail ]"
                 ></v-text-field>
 
               <v-text-field
@@ -51,9 +52,14 @@
                 :append-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
                 @click:append="() => (visible = !visible)"
                 :type="visible ? 'text' : 'password'"
+                :persistent-hint="explanation"
+                :hint="hint"
+                @focus="explanation = true"
+                @blur="explanation = false"
+                :rules="[ validatePassword ]"
                 ></v-text-field>
             </v-form>
-            <div class="caption align-left">By clicking “Sign up” below, you agree to our <router-link to="/terms">Terms of Service</router-link> and <router-link to="/privacy">Privacy Statement</router-link>. We’ll occasionally send you account-related emails.</div>
+            <div class="caption align-left pt-4">By clicking “Sign up” below, you agree to our <router-link to="/terms">Terms of Service</router-link> and <router-link to="/privacy">Privacy Statement</router-link>. We’ll occasionally send you interview reminders and account-related emails.</div>
           </v-card-text>
           <v-card-actions>
             <span class="caption pl-2">Already have an account?
@@ -88,16 +94,90 @@ export default {
       email: '',
       company: '',
       password: '',
+      hint: '',
       visible: false,
-      loading: false
+      loading: false,
+      explanation: false
     };
   },
   methods: {
-    pressEnter () {
-      console.log('enter');
+    pressEnter (event) {
+      if (this.name && this.email && this.password) {
+        this.signup();
+      } else if (event.target === this.$refs.name.$refs.input) {
+        this.$refs.email.focus();
+      } else if (event.target === this.$refs.email.$refs.input) {
+        this.$refs.company.focus();
+      } else if (event.target === this.$refs.company.$refs.input) {
+        this.$refs.password.focus();
+      } else if (event.target === this.$refs.password.$refs.input) {
+        this.$refs.name.focus();
+      }
     },
     signup () {
-      console.log('sign up');
+      if (this.$refs.form.validate()) {
+        this.loading = true;
+      }
+    },
+    passwordHint () {
+      const hints = [];
+      let valid = true;
+
+      if (/[A-Z]/.test(this.password)) {
+        hints.push('<s>Uppercase Letter</s>');
+      } else {
+        hints.push('Uppercase Letter');
+        valid = false;
+      }
+
+      if (/[a-z]/.test(this.password)) {
+        hints.push('<s>Lowercase Letter</s>');
+      } else {
+        hints.push('Lowercase Letter');
+        valid = false;
+      }
+
+      if (/[0-9]/.test(this.password)) {
+        hints.push('<s>Number</s>');
+      } else {
+        hints.push('Number');
+        valid = false;
+      }
+
+      if (/[!@#$%^&*(),.?":{}|<>]/.test(this.password)) {
+        hints.push('<s>Special Character</s>');
+      } else {
+        hints.push('Special Character');
+        valid = false;
+      }
+
+      if (this.password.length >= 8) {
+        hints.push('<s>Eight Character Minimum</s>');
+      } else {
+        hints.push('Eight Character Minimum');
+        valid = false;
+      }
+
+      this.hint = hints.join(', ');
+      return valid;
+    },
+    validateName (value = '') {
+      if (!/^\w+\s+\w+$/.test(value)) {
+        return 'Please enter a first and last name';
+      }
+      return true;
+    },
+    validateEmail (value = '') {
+      if (!/^[^]+@[^]+\.[^]+$/.test(value)) {
+        return 'Please enter a valid email address';
+      }
+      return true;
+    },
+    validatePassword (value = '') {
+      if (!this.passwordHint()) {
+        return this.hint;
+      }
+      return true;
     }
   }
 };
