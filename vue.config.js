4921f264619ceafa2c16eea8a17d74525a6d1965
Mark Barr <mark@hyperingenuity.com>
2019-10-01 16:48:21 -0400

    interrogative: switch to mjml for email templates, refactors

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,17 @@
 const config = require('./defaults');
 
-module.exports = { devServer: {
-  host: '0.0.0.0',
-  public: '0.0.0.0:8080',
-  disableHostCheck: true,
-  proxy: {
-    '^/api': { target: `http://localhost:${ config.manager.port }` },
-    '^/ws': {
-      target: `ws://localhost:${ config.manager.port }`,
-      ws: true
+module.exports = {
+  devServer: {
+    host: '0.0.0.0',
+    public: '0.0.0.0:8080',
+    disableHostCheck: true,
+    proxy: {
+      '^/api': { target: `http://localhost:${ config.manager.port }` },
+      '^/ws': {
+        target: `ws://localhost:${ config.manager.port }`,
+        ws: true
+      }
     }
-  }
-} };
+  },
+  filenameHashing: process.env.NODE_ENV !== 'production'
+};
