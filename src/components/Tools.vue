59619dfa2afcf86088a20e55346180c649128f5a
Mark Barr <mark@hyperingenuity.com>
2019-10-11 18:19:27 -0400

    tools: client side wiring and layout

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -1,9 +1,19 @@
 <template>
 <div class="tools pl-4">
-  <v-btn small><v-icon small left>mdi-file</v-icon> Clear Open Files</v-btn><br>
-  <v-btn small><v-icon small left>mdi-cloud-upload</v-icon>Clear Uploads</v-btn><br>
-  <v-btn small><v-icon small left>mdi-forum</v-icon>Clear Chat</v-btn><br>
-  <v-btn small><v-icon small left>mdi-close</v-icon>End Interview</v-btn>
+  <v-btn small @click="clearOpenFiles">
+    <v-icon small left>mdi-file</v-icon> Clear Open Files
+  </v-btn><br>
+  <v-btn small @click="clearUploadsList">
+    <v-icon small left>mdi-cloud-upload</v-icon>
+    Clear Uploads List
+  </v-btn><br>
+  <v-btn small @click="clearChatHistory">
+    <v-icon small left>mdi-forum</v-icon>
+    Clear Chat History
+  </v-btn><br>
+  <v-btn small color="red" class="mt-9" @click="endInterview">
+    <v-icon small left>mdi-close-circle-outline</v-icon>End Interview
+  </v-btn>
 </div>
 </template>
 
@@ -14,6 +24,32 @@ export default {
   name: 'tools',
   data () {
     return { state };
+  },
+  methods: {
+    clearOpenFiles () {
+      this.$events.emit({
+        type: 'tools:files:clear-open',
+        data: { auth: this.state.user }
+      });
+    },
+    clearUploadsList () {
+      this.$events.emit({
+        type: 'tools:files:clear-uploads',
+        data: { auth: this.state.user }
+      });
+    },
+    clearChatHistory () {
+      this.$events.emit({
+        type: 'tools:messages:clear',
+        data: { auth: this.state.user }
+      });
+    },
+    endInterview () {
+      this.$events.emit({
+        type: 'tools:interview:end',
+        data: { auth: this.state.user }
+      });
+    }
   }
 };
 </script>
